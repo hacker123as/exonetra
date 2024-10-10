7004d97bf51fbebc1168947617898e385163c21a
hacker123as <73672409+hacker123as@users.noreply.github.com>
2024-10-10 15:44:29 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Simple form submit handler (just for demonstration purposes)
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Your message has been sent!');
+});
